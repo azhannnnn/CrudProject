09559a25aec27297d9bbf4a7fa4cfe47742ac6e6
Azhan Khan <azhanborntowin22@gmail.com>
2024-02-02 11:28:29 +0530

    modify

--- project/app/urls.py
+++ project/app/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path,include
+from .views import *
 
 urlpatterns = [
+
+    path('',home,name="home"),
+    path('registerpage/',registerpage,name='registerpage'),
+    path('loginpage/',loginpage,name='loginpage'),
     
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def registerpage(request):
+    return render(request,'register.html')
+
+def loginpage(request):
+    return render(request,'login.html')
